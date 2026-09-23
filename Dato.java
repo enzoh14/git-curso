cd2a5caad1887fe2f19896f86ccc030ae1c2eb69
Enzo Miranda <enzomiranda1405@gmail.com>
2026-09-23 18:59:30 -0300

    feat:edite el mensaje de error

--- Dato.java
+++ Dato.java
@@ -132,7 +132,7 @@ public class Dato {
                }
            }
        } catch (FileNotFoundException e) {
-           System.out.println("ENZOOO No se encontró el archivo especificado: " + fileName);
+           System.out.println("Enzo No se encontró el archivo especificado: " + fileName);
            throw e; // 
        }
             
